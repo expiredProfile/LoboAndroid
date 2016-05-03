e7dbbb3f256068ac0c0db793deddf9a4995fb759
Kasper <kasper.stenroos@metropolia.fi>
2016-05-03 18:55:41 +0300

    for push

--- app/src/main/java/com/metropolia/kim/loboandroid/NetworkingTask.java
+++ app/src/main/java/com/metropolia/kim/loboandroid/NetworkingTask.java
@@ -122,6 +122,7 @@ public class NetworkingTask extends AsyncTask<String, String, String> {
                     break;
 
                 case "alert":
+                    /*
                     AlertXmlParser alertParser = new AlertXmlParser();
                     List<Alert> alerts = alertParser.parse(is);
                     for (Alert a : alerts) {
@@ -135,6 +136,7 @@ public class NetworkingTask extends AsyncTask<String, String, String> {
                         Uri uri = Uri.parse(ChatProvider.URL + "/alerts/insert");
                         this.context.getContentResolver().insert(uri, values);
                     }
+                    */
                     break;
             }
             is.close();
